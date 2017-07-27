9f9d4a377004c9f5316df1484dfcdca0b5b978f5
Administrator <Administrator@Pengjunlin>
2017-07-27 12:08:36 +0800

    Add SQL parser code.

---
+++ src/main/java/com/boonya/sql/parser/DeleteSqlParser.java
@@ -0,0 +1,13 @@
+package com.boonya.sql.parser;
+
+public class DeleteSqlParser extends BaseSingleSqlParser {
+	public DeleteSqlParser(String originalSql) {
+		super(originalSql);
+	}
+
+	@Override
+	protected void initializeSegments() {
+		segments.add(new SqlSegment("(delete from)(.+)( where | ENDOFSQL)", "[,]"));
+		segments.add(new SqlSegment("(where)(.+)( ENDOFSQL)", "(and|or)"));
+	}
+}
