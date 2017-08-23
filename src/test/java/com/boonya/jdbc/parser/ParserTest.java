3727b7b55027559643dd36675522ebe54bc58069
Administrator <Administrator@Pengjunlin>
2017-08-23 11:27:35 +0800

    Update ParserTest.java

--- src/test/java/com/boonya/jdbc/parser/ParserTest.java
+++ src/test/java/com/boonya/jdbc/parser/ParserTest.java
@@ -8,20 +8,52 @@ import com.boonya.sql.parser.util.SqlParserUtil;
 public class ParserTest {
 
 	@Test
-	public void parseSelect() {
-		// TODO Auto-generated method stub
-		// String test="select a from b " +
-		// "\n"+"where a=b";
-		// test=test.replaceAll("\\s{1,}", " ");
-		// System.out.println(test);
-		// 程序的入口
-		String testSql = "select c1,c2,c3 from t1,t2 where condi3=3  or condi4=5 order by o1,o2";
+	public void parseSelectSQL() {
+		String sql = "select c1,c2,c3 from t1,t2 where condi3=3  or condi4=5 order by o1,o2";
+		System.out.println(sql);
 		SqlParserUtil test = new SqlParserUtil();
-		String result = test.getParsedSql(testSql);
+		String result = test.getParsedSql(sql);
 		System.out.println(result);
 		//保存解析结果
-		List<SqlSegment> resultList=test.getParsedSqlList(testSql);
+		List<SqlSegment> resultList=test.getParsedSqlList(sql);
 		System.out.println(resultList);
 	}
+	
+	@Test
+	public void parseInsertSQL(){
+		String sql = "insert into user (field_a,field_b) values ('a','b')"; 
+		System.out.println(sql);
+		SqlParserUtil test = new SqlParserUtil();
+		String result = test.getParsedSql(sql);
+		System.out.println(result);
+		//保存解析结果
+		List<SqlSegment> resultList=test.getParsedSqlList(sql);
+		System.out.println(resultList);
+	}
+	
+	@Test
+	public void parseDeleteSQL(){
+		String sql = "delete from user where id=1"; 
+		System.out.println(sql);
+		SqlParserUtil test = new SqlParserUtil();
+		String result = test.getParsedSql(sql);
+		System.out.println(result);
+		//保存解析结果
+		List<SqlSegment> resultList=test.getParsedSqlList(sql);
+		System.out.println(resultList);
+	}
+	
+	@Test
+	public void parseUpdateSQL(){
+		String sql = "update user set name='boonya' where id=1"; 
+		System.out.println(sql);
+		SqlParserUtil test = new SqlParserUtil();
+		String result = test.getParsedSql(sql);
+		System.out.println(result);
+		//保存解析结果
+		List<SqlSegment> resultList=test.getParsedSqlList(sql);
+		System.out.println(resultList);
+	}
+
 
 }
